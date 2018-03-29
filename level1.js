184c0d72e657f410d6f63c8a83a3620239efa222
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-03-28 23:42:25 -0400

    JS101 exercises

---
+++ level1.js
@@ -0,0 +1,44 @@
+function lvl1exercise1() {
+	// Declare a variable without instantiating it and return it!
+	var myVariable;
+	return myVariable;
+}
+
+function lvl1exercise2() {
+	// Declare and instantiate a number and return it
+	var myNumber = 2;
+	return myNumber;
+}
+
+function lvl1exercise3() {
+	// Declare and instantiate a floating point number that isn't a whole number and return it
+	var myFloat = 4.5;
+	return myFloat;	
+}
+
+function lvl1exercise4() {
+	// Declare and instantiate a string "Hello World!" and return it
+	var myStr = "Hello World!";
+	return myStr;
+}
+
+function lvl1exercise5() {
+	// Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+	var myArray = ["Hello World!", 4];
+	return myArray;
+}
+
+function lvl1exercise6() {
+	// Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+	var myObj = {
+		key1: "Hello World!",
+		key2: 4
+	};
+	return myObj;
+}
+
+function lvl1exercise7() {
+	// Declare and instantiate a boolean value 'false' and return it
+	var myBool = false;
+	return myBool;
+}
---
+++ level3.js
@@ -0,0 +1,32 @@
+function lvl3exercise1() {
+	// Create a "hello" and a "world", return the concatenation of the two
+	var str1 = "hello";
+	var str2 = "world";
+	return str1 + str2;
+}
+
+function lvl3exercise2() {
+	// Create a "hello" and a 12, return the concatenation of the two
+	var ex2var1 = "hello";
+	var ex2var2 = 12;
+	return ex2var1 + ex2var2;
+}
+
+function lvl3exercise3() {
+	// Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+	var ex3var1 = 12;
+	return ex3var1 + "";
+}
+
+function lvl3exercise4() {
+	// Create a "hello world!" string. Return the length of the string
+	var ex4var1 = "hello world!";
+	return ex4var1.length;
+}
+
+function lvl3exercise5() {
+	// Create a "hello world!" string. Return the index of the word "world".
+	var ex5var1 = "hello world!";
+	return ex5var1.indexOf("world");
+}
+
